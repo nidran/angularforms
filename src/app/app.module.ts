6b00151a15a1b5836e916af836bade5840989bd5
nidran <nidhi.ranjan145@gmail.com>
2020-06-28 12:59:13 +0530

    ngx-bootstrap buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { UserSettingsFormComponent } from './user-settings-form/user-settings-form.component';
-
+import {ButtonsModule} from 'ngx-bootstrap/buttons';
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,7 +13,8 @@ import { UserSettingsFormComponent } from './user-settings-form/user-settings-fo
   imports: [
     BrowserModule, 
     FormsModule, 
-    HttpClientModule
+    HttpClientModule,
+    ButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -18,6 +18,7 @@ export class UserSettingsFormComponent implements OnInit {
     notes:null
   };
 
+  singleModel ="On";
   userSettings : UserSettings = {...this.originalUserSettings};
   postError = false;
   postErrorMessage = " ";
