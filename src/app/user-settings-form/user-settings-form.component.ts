eaf7c808580b6120b34cc760b5fcac79dfbcd7cf
nidran <nidhi.ranjan145@gmail.com>
2020-06-28 11:47:50 +0530

    onSubmit() errors

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {UserSettings} from "../data/user-settings"; 
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
@@ -19,6 +20,10 @@ export class UserSettingsFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  
+  onSubmit(form: NgForm){
+    console.log('in onsubmit;',  form.valid);
   }
 
 }
