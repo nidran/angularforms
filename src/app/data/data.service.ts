3db35fddddf5e3b0779f7a92e0034864db5046d2
nidran <nidhi.ranjan145@gmail.com>
2020-06-28 12:12:48 +0530

    http client

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { UserSettings } from './user-settings';
 import { Observable, of } from 'rxjs';
 import { UserSettingsFormComponent } from '../user-settings-form/user-settings-form.component';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
-  postUserSettingsForm( userSettings : UserSettings) :Observable<UserSettings>{
- return of(userSettings);
+  postUserSettingsForm( userSettings : UserSettings) :Observable<any>{
+//  return of(userSettings);
+    return this.http.post('url', userSettings);
   }
 }
